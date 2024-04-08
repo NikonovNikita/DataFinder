eaaf36be274af92178680c4ed41372258c8f237c
NikonovNikita <jassie.jassie@yandex.ru>
2024-04-08 15:50:36 +0400

    Разделенная на проекты трехслойка

---
+++ DataFinder.BLL/FileService.cs
@@ -0,0 +1,36 @@
+﻿using DataFinder.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFinder.BLL;
+
+public class FileService
+{
+    public FileRepository FileRepository { get; }
+
+    public FileService()
+    {
+        FileRepository = new FileRepository();
+    }
+
+    public IEnumerable<File> GetAllFiles()
+    {
+        return FileRepository.GetFilesByDirectory().ToList().Select(fileEntity => new File(fileEntity));
+    }
+
+    public File GetFileByFileName(string fileName)
+    {
+        if (string.IsNullOrEmpty(fileName))
+            throw new BusinessRuleException("Введите корректное значение!");
+
+        var fileEntity = FileRepository.GetFileByFileName(fileName);
+
+        if (fileEntity == null)
+            throw new BusinessRuleException("Файл с указанным именем не обнаружен!");
+
+        return new File(fileEntity);
+    }
+}
---
+++ DataFinder/PLL/Program.cs
@@ -0,0 +1,41 @@
+﻿using DataFinder.BLL;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fileService = new FileService();
+
+        while (true)
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("DataFinder v.1.0");
+            Console.WriteLine("Отобразить все файлы --> 1");
+            Console.WriteLine("Выполнить поиск файла по имени --> 2");
+
+            string input = Console.ReadLine();
+
+            if(input == "1")
+            {
+                fileService.GetAllFiles().ToList().ForEach(file =>
+                Console.WriteLine($"Идентификатор: {file.Id}\nИмя файла: {file.Name}\nПуть файла: {file.Path}"));
+            }
+
+            if(input == "2")
+            {
+                Console.WriteLine("Введите имя файла, который хотите найти");
+                string fileNameInput = Console.ReadLine();
+
+                try
+                {
+                    var file = fileService.GetFileByFileName(fileNameInput);
+                    Console.WriteLine($"Идентификатор: {file.Id}\nИмя файла: {file.Name}\nПуть файла: {file.Path}");
+                }
+                catch(BusinessRuleException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
